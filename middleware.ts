854e1e878ff78b7f6317fd480b92d0f847b29600
jokerleon <375577939@qq.com>
2023-04-28 22:34:34 +0800

    azure修复

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { getServerSideConfig } from "./app/config/server";
 import md5 from "spark-md5";
 
 export const config = {
-  matcher: ["/api/openai", "/api/chat-stream"],
+  matcher: ["/api/openai", "/api/chat-stream","/api/chat-completion"],
 };
 
 const serverConfig = getServerSideConfig();
