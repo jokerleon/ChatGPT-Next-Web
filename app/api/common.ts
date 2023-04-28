5e4794f9f263ab8769f9b9222498bd7d0e522a6d
jokerleon <375577939@qq.com>
2023-04-28 23:04:39 +0800

    azure修复

--- app/api/common.ts
+++ app/api/common.ts
@@ -6,18 +6,27 @@ const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
 export async function requestOpenai(req: NextRequest) {
-  let apiBaseUrl = process.env.AZURE_OPENAI_API_BASE_URL;
-  const version = "2023-03-15-preview";
-  const deployment = process.env.AZURE_OPENAI_DEPLOYMENT || "";
-  if (apiBaseUrl && apiBaseUrl.endsWith("/")) {
-    apiBaseUrl = apiBaseUrl.slice(0, -1);
-  }
-  let baseUrl = `${apiBaseUrl}/openai/deployments/${deployment}/chat/completions?api-version=${version}`;
-  let apiKey = process.env.AZURE_OPENAI_API_KEY || "";
-  let openaiPath = req.headers.get("path");
-  let token = req.headers.get("token");
-  let finalUrl = baseUrl;
-  if (token && token.length > 0) {
+  let baseUrl: string;
+  let apiKey: string;
+  let openaiPath: string;
+  let finalUrl: string;
+
+  const useAzureOpenAI =
+    process.env.AZURE_OPENAI_API_BASE_URL &&
+    process.env.AZURE_OPENAI_API_BASE_URL.length > 0;
+  if (useAzureOpenAI) {
+    let apiBaseUrl = process.env.AZURE_OPENAI_API_BASE_URL;
+    const version = "2023-03-15-preview";
+    const deployment = process.env.AZURE_OPENAI_DEPLOYMENT || "";
+    if (apiBaseUrl && apiBaseUrl.endsWith("/")) {
+      apiBaseUrl = apiBaseUrl.slice(0, -1);
+    }
+
+    baseUrl = `${apiBaseUrl}/openai/deployments/${deployment}/chat/completions?api-version=${version}`;
+    apiKey = process.env.AZURE_OPENAI_API_KEY || "";
+    openaiPath = req.headers.get("path") || "";
+    finalUrl = baseUrl;
+  } else {
     apiKey = req.headers.get("token") || "";
     openaiPath = req.headers.get("path") || "";
 
@@ -29,6 +38,7 @@ export async function requestOpenai(req: NextRequest) {
     baseUrl = `${PROTOCOL}://${baseUrl}`;
   }
 
+  console.log("[Token] ", apiKey);
   console.log("[Proxy] ", openaiPath);
   console.log("[Base Url]", baseUrl);
   console.log("[Final Url]", finalUrl);
